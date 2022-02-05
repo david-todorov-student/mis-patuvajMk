0dba8c106402e3f05500fb7c9a02a756ecf0956a
Ana Gashtarova <63113172+anagashtarova@users.noreply.github.com>
2022-02-05 21:12:21 +0100

    Adding changes

--- patuvaj_mk/lib/item.dart
+++ patuvaj_mk/lib/item.dart
@@ -1,21 +1,6 @@
-// import 'package:flutter/material.dart';
-
-// class App extends StatelessWidget {
-//   final String startText;
-//   final String stopText;
-//   App(this.startText, this.stopText);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text(startText)),
-//       body: Container(decoration: BoxDecoration(color: Colors.black)),
-//     );
-//   }
-// }
-//
 import 'package:flutter/material.dart';
 import './uspeshno.dart';
+import './mapa.dart';
 
 class MyAppp extends StatelessWidget {
   final String name;
@@ -25,31 +10,38 @@ class MyAppp extends StatelessWidget {
   final String endTime;
   final String startDest;
   final String endDest;
+  final String adr1;
+  final String adr2;
+  final String kola;
+  final String boja;
+  String? selectedDat;
+  String? brPatnici;
+  String? price;
 
-  MyAppp(this.startDest, this.endDest, this.name, this.stars, this.image, this.startTime, this.endTime);
+  MyAppp(this.price, this.brPatnici, this.selectedDat, this.kola, this.boja, this.adr1, this.adr2, this.startDest, this.endDest, this.name, this.stars, this.image, this.startTime, this.endTime);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Преглед'),
+        title: Text('Преглед'),
         // leading: getBackButton(),
       ),
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(8.0),
         child: RaisedButton(
-          padding: const EdgeInsets.only(top: 23.0, bottom: 23.0),
+          padding: EdgeInsets.only(top: 23.0, bottom: 23.0),
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
           onPressed: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => Succ(name.toString(), stars.toString(), image.toString(), startTime.toString(), endTime.toString()),
+                builder: (context) => Succ(price.toString(), brPatnici.toString(), selectedDat.toString(), kola.toString(), boja.toString(), adr1.toString(), adr2.toString(), startDest.toString(), endDest.toString(), name.toString(), stars.toString(), image.toString(), startTime.toString(), endTime.toString()),
               ),
             );
           },
           color: Colors.black,
           textColor: Colors.white,
-          child: const Text(
+          child: Text(
             'Резервирај',
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
@@ -57,127 +49,126 @@ class MyAppp extends StatelessWidget {
       ),
       body: SafeArea(
         child: ListView(
-          padding: const EdgeInsets.all(8.0),
+          padding: new EdgeInsets.all(8.0),
           children: [
             Card(
               color: Colors.grey[100],
               child: Padding(
                 padding: const EdgeInsets.all(0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
-                      crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
-                      children: const [
+                child: Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
+                        crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
+                        children: [
+                          Text(
+                            selectedDat.toString(),
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                        ],
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
+                          crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
+
+                          children: [
+                            Text(
+                              startTime,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(top: 8),
+                              width: 100,
+                              color: Colors.black54,
+                              height: 1,
+                            ),
+                            // const SizedBox(height: 4),
+                            Text(
+                              endTime,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
+                          crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
+
+                          children: [
+                            Text(
+                              startDest,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            //SizedBox(height: 100),
+                            Container(width: 100, color: Colors.transparent),
+                            Text(
+                              endDest,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ]),
+                      SizedBox(height: 30),
+                      Row(mainAxisSize: MainAxisSize.min, children: [
                         Text(
-                          '10 Okt, 2021',
+                          adr1,
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Container(width: 100, color: Colors.transparent),
+                        Text(
+                          adr2,
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ]),
+                      SizedBox(height: 30),
+                      Row(children: [
+                        Text(
+                          "Вкупна цена за " + this.brPatnici.toString() + " патници",
                           style: TextStyle(
                             fontSize: 16,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          (int.parse(this.brPatnici.toString()) * int.parse(this.price.toString().split(" ")[0])).toString() + " ден.",
+                          style: TextStyle(
+                            fontSize: 20,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                      ],
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
-                        crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
-
-                        children: [
-                          Text(
-                            startTime,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 8),
-                            width: 100,
-                            color: Colors.black54,
-                            height: 1,
-                          ),
-                          // const SizedBox(height: 4),
-                          Text(
-                            endTime,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ]),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
-                        crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
-
-                        children: [
-                          Text(
-                            startDest,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.black54,
-                            ),
-                          ),
-                          //SizedBox(height: 100),
-                          Container(width: 100, color: Colors.transparent),
-                          Text(
-                            endDest,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.black54,
-                            ),
-                          ),
-                        ]),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
-                        crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
-
-                        children: [
-                          const Text(
-                            "08:00",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black54,
-                            ),
-                          ),
-                          //SizedBox(height: 100),
-                          Container(width: 100, color: Colors.transparent),
-                          const Text(
-                            "08:00",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black54,
-                            ),
-                          ),
-                        ]),
-                    const SizedBox(height: 30),
-                    Row(children: const [
-                      Text(
-                        "Вкупна цена за x патници",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      Spacer(),
-                      Text(
-                        "300 ден.",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      //SizedBox(height: 100),
-                    ]),
-                  ],
+                        //SizedBox(height: 100),
+                      ]),
+                    ],
+                  ),
                 ),
               ),
             ),
             // CarBlock(widget.car),
             // PickupInfoWidget(widget.car),
             // MapWidget(widget.car),
-            const SizedBox(
+            SizedBox(
               height: 5,
             ),
             Padding(
@@ -197,30 +188,30 @@ class MyAppp extends StatelessWidget {
                       ),
                     ],
                   ),
-                  const SizedBox(width: 30),
+                  SizedBox(width: 30),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Text(
                         name,
-                        style: const TextStyle(color: Colors.black, fontSize: 18),
+                        style: TextStyle(color: Colors.black, fontSize: 18),
                       ),
                       Row(
                         children: <Widget>[
-                          const Icon(
+                          Icon(
                             Icons.star,
                             color: Colors.grey,
                           ),
                           Text(
                             stars,
-                            style: const TextStyle(color: Colors.grey),
+                            style: TextStyle(color: Colors.grey),
                           ),
                         ],
                       ),
                     ],
                   ),
 
-                  const SizedBox(
+                  SizedBox(
                     height: 33,
                     child: VerticalDivider(
                       color: Colors.grey,
@@ -234,36 +225,44 @@ class MyAppp extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Text(
-                        "test".toUpperCase(),
-                        style: const TextStyle(color: Colors.black, fontSize: 18),
+                        kola.toUpperCase(),
+                        style: TextStyle(color: Colors.black, fontSize: 18),
                       ),
-                      const Text(
-                        "test",
+                      Text(
+                        boja,
                         style: TextStyle(color: Colors.black, fontSize: 12),
                       )
                     ],
                   ),
-                  const SizedBox(width: 30),
+                  SizedBox(width: 20),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const <Widget>[
+                    children: <Widget>[
                       Icon(Icons.directions_car),
                     ],
                   ),
                 ],
               ),
             ),
-            const SizedBox(
+            SizedBox(
               height: 5,
             ),
 
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(20.0),
               child: Row(
-                children: const <Widget>[
-                  Text(
-                    'Преглед на локациите',
-                    //     style: kButtonTextStyle,
+                children: <Widget>[
+                  FlatButton(
+                    color: Colors.orange,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => MapSample()));
+                    },
+                    child: Text(
+                      'Преглед на дестинацијата',
+                    ),
                   ),
                 ],
               ),
@@ -273,17 +272,26 @@ class MyAppp extends StatelessWidget {
               child: FlatButton(
                 color: Colors.tealAccent[700],
                 textColor: Colors.white,
-                padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+                padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                onPressed: () {
-                  // Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  //   return VoucherScreen(
-                  //     reservationNumber: widget.car.reservationNumber,
-                  //     email: widget.car.customerEmail,
-                  //   );
-                  // }));
-                },
-                child: const Text(
+                onPressed: () => showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Контакт телефон'),
+                    content: const Text('075/234 654'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: const Text('Cancel'),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'OK'),
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                ),
+                child: Text(
                   'Исконтактирај го превозникот',
                 ),
               ),
