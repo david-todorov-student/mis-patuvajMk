0025c20a902386dc5609125e609511ea6d200ee3
Ana Gashtarova <63113172+anagashtarova@users.noreply.github.com>
2022-02-05 21:11:37 +0100

    Adding changes

--- patuvaj_mk/lib/baraj.dart
+++ patuvaj_mk/lib/baraj.dart
@@ -5,7 +5,7 @@ import 'dart:async';
 import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: NewScreen(),
   ));
@@ -25,19 +25,10 @@ class Item {
 }
 
 class NewScreen extends StatefulWidget {
-  const NewScreen({Key? key}) : super(key: key);
-
-  @override
   State createState() => NewScreenState();
 }
 
 class NewScreenState extends State<NewScreen> {
-  List<Person> persons = [
-    Person(name: 'Bill Will', profileImg: 'img/pic-1.png', bio: "Software Developer"),
-    Person(name: 'Andy Smith', profileImg: 'img/pic-2.png', bio: "UI Designer"),
-    Person(name: 'Creepy Story', profileImg: 'img/pic-3.png', bio: "Software Tester")
-  ];
-
   Item? selectedUser;
 
   List<Item> users = <Item>[
@@ -46,35 +37,36 @@ class NewScreenState extends State<NewScreen> {
     const Item('3'),
     const Item('4'),
   ];
-  String? _selectedColor;
 
   DateTime selectedDate = DateTime.now();
 
   Future<void> _selectDate(BuildContext context) async {
     DateTime? picked = await showDatePicker(context: context, initialDate: selectedDate, firstDate: DateTime(2015, 8), lastDate: DateTime(2101));
-    if (picked != null && picked != selectedDate) {
+    if (picked != null && picked != selectedDate)
       setState(() {
         selectedDate = picked;
       });
-    }
   }
+
+  TextEditingController startTextEditingController = TextEditingController();
+  TextEditingController endTextEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Барај')),
+    return new Scaffold(
+      appBar: AppBar(title: Text('Барај')),
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: const AssetImage("assets/bb.jpg"),
-            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("assets/bb.jpg"),
+            colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
             fit: BoxFit.cover,
           ),
         ),
         child:
             // Row(
             //   children: <Widget>[
-            Form(
+            new Form(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
@@ -89,13 +81,15 @@ class NewScreenState extends State<NewScreen> {
                 //     ),
                 //   ),),
                 // ),
-                const SizedBox(
+                SizedBox(
                   height: 20,
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(0.0, 0.0, 64.0, 8.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    controller: startTextEditingController,
+                    decoration: InputDecoration(
                       icon: Icon(Icons.location_on, color: Colors.grey),
                       labelText: "Почетна дестинација...",
                     ),
@@ -103,8 +97,10 @@ class NewScreenState extends State<NewScreen> {
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(0.0, 0.0, 64.0, 8.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    controller: endTextEditingController,
+                    decoration: InputDecoration(
                       icon: Icon(Icons.location_on, color: Colors.grey),
                       labelText: "Крајна дестинација...",
                     ),
@@ -123,15 +119,15 @@ class NewScreenState extends State<NewScreen> {
                         // ),
                         child: Row(
                           children: <Widget>[
-                            const Icon(
+                            Icon(
                               Icons.date_range,
                               color: Colors.grey,
                             ),
-                            const SizedBox(
+                            SizedBox(
                               width: 15,
                             ),
                             Text(
-                              DateFormat.yMMMd().format(selectedDate),
+                              DateFormat.yMMMd().format(this.selectedDate),
                             ),
                           ],
                         ),
@@ -158,7 +154,7 @@ class NewScreenState extends State<NewScreen> {
 
                     // SfDateRangePicker(),
                     SizedBox.fromSize(
-                      size: const Size(26, 26), // button width and height
+                      size: Size(26, 26), // button width and height
                       child: ClipOval(
                         child: Material(
                           color: Colors.white, // button color
@@ -167,7 +163,7 @@ class NewScreenState extends State<NewScreen> {
                             onTap: () => _selectDate(context), // button pressed
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
-                              children: const <Widget>[
+                              children: <Widget>[
                                 Icon(Icons.add_circle_outline), // icon
                               ],
                             ),
@@ -212,37 +208,41 @@ class NewScreenState extends State<NewScreen> {
                 //     ),
                 //   ),
                 // ),
+                //ovdeeeeeeee
+
                 Container(
                   padding: const EdgeInsets.fromLTRB(0.0, 10.0, 64.0, 5.0),
                   child: Row(
                     children: <Widget>[
-                      const Icon(
+                      Icon(
                         Icons.people_alt,
                         color: Colors.grey,
                       ),
-                      const SizedBox(
+                      SizedBox(
                         width: 15,
                       ),
+                      //  ov de
+
                       DropdownButton<Item>(
                         underline: Container(
                           height: 2,
                         ),
-                        hint: const Text("Патници"),
+                        hint: Text("Патници"),
                         value: selectedUser,
                         onChanged: (Item? Value) {
                           setState(() {
                             selectedUser = Value;
                           });
                         },
-                        items: users.map((Item user) {
+                        items: users.map((Item? user) {
                           return DropdownMenuItem<Item>(
                             value: user,
                             child: Container(
                               width: 200, // Container child widget will get this width value
-                              //height: myHeightValue, // C
+                              //height: myHeghtValue, // C
                               alignment: Alignment.centerLeft,
                               child: Text(
-                                user.name,
+                                user!.name,
                                 style: TextStyle(fontSize: 16, color: Colors.grey[600]),
                               ),
                             ),
@@ -274,10 +274,10 @@ class NewScreenState extends State<NewScreen> {
                   color: Colors.black54,
                   height: 0.5,
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 35,
                 ),
-                SizedBox(
+                new SizedBox(
                   width: 200.0,
                   height: 50.0,
                   child: ElevatedButton(
@@ -286,7 +286,22 @@ class NewScreenState extends State<NewScreen> {
                       style: TextStyle(color: Colors.white),
                     ),
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CardScreen()));
+                      if (selectedUser == null || startTextEditingController.text.isEmpty || endTextEditingController.text.isEmpty) {
+                        showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            content: const Text('Сите полиња се задолжителни'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, 'OK'),
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => CardScreen(brPatnici: selectedUser!.name.toString(), selectedDat: DateFormat.yMMMd().format(this.selectedDate).toString())));
+                      }
                     },
                     style: ElevatedButton.styleFrom(
                       primary: Colors.black,
@@ -296,11 +311,6 @@ class NewScreenState extends State<NewScreen> {
                       ),
                     ),
                   ),
-                ),
-                Image.asset(
-                  "assets/1.png",
-                  height: 100.0,
-                  fit: BoxFit.cover,
                 ),
               ],
             ),
@@ -314,78 +324,3 @@ class NewScreenState extends State<NewScreen> {
     );
   }
 }
-
-// class MyHome extends StatefulWidget {
-//   @override
-//   _MyHomeState createState() => _MyHomeState();
-// }
-
-// class _MyHomeState extends State<MyHome> {
-//   List<Person> persons = [
-//     Person(name: 'Bill Will', profileImg: 'img/pic-1.png', bio: "Software Developer"),
-//     Person(name: 'Andy Smith', profileImg: 'img/pic-2.png', bio: "UI Designer"),
-//     Person(name: 'Creepy Story', profileImg: 'img/pic-3.png', bio: "Software Tester")
-//   ];
-
-//   Widget personDetailCard(Person) {
-//     return Padding(
-//       padding: const EdgeInsets.all(10.0),
-//       child: Card(
-//         color: Colors.grey[800],
-//         child: Padding(
-//           padding: const EdgeInsets.all(8.0),
-//           child: Row(
-//             children: <Widget>[
-//               Padding(
-//                 padding: const EdgeInsets.all(8.0),
-//                 child: Container(width: 50.0, height: 50.0, decoration: new BoxDecoration(shape: BoxShape.circle, image: new DecorationImage(fit: BoxFit.cover, image: AssetImage(Person.profileImg)))),
-//               ),
-//               Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: <Widget>[
-//                   Text(
-//                     Person.name,
-//                     style: TextStyle(color: Colors.white, fontSize: 18),
-//                   ),
-//                   Text(
-//                     Person.bio,
-//                     style: TextStyle(color: Colors.white, fontSize: 12),
-//                   )
-//                 ],
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.grey[900],
-//       body: Padding(
-//         padding: const EdgeInsets.fromLTRB(10, 50, 10, 10),
-//         child: Column(
-//           children: <Widget>[
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               children: <Widget>[
-//                 Icon(Icons.menu, size: 35, color: Colors.white),
-//                 Text(
-//                   'Notifications',
-//                   style: TextStyle(color: Colors.white, fontSize: 25),
-//                 ),
-//                 Icon(Icons.notifications_none, size: 35, color: Colors.white)
-//               ],
-//             ),
-//             Column(
-//                 children: persons.map((p) {
-//               return personDetailCard(p);
-//             }).toList())
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
